31092e2d9dea1ef9e0bbd8b6ef2d4f3b65b336d0
AnonX9 <avaikamarcel@gmail.com>
2024-03-23 17:13:28 +0100

    add a splash screen

---
+++ lib/src/splash/presentation/pages/splash_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../router/config.routes.dart';
+import '../../../shared/prsentation/widgets/standard_error_widget.dart';
+import '../../../shared/prsentation/widgets/standard_loading_widget.dart';
+import '../providers/splash_loading.provider.dart';
+
+class SplashPage extends ConsumerWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final loading = ref.watch(splashLoadingProvider);
+
+    return Scaffold(
+      body: Center(
+        child: switch (loading) {
+          AsyncData() => const Icon(Icons.sentiment_very_satisfied)
+              .animate()
+              .fadeIn(duration: 200.milliseconds)
+              .fadeOut(duration: 800.milliseconds)
+              .then()
+              .callback(callback: (value) => const HomeRoute().go(context)),
+          AsyncError(:final error, :final stackTrace) =>
+            StandardErrorWidget(error, stackTrace),
+          _ => const StandardLoadingWidget()
+        },
+      ),
+    );
+  }
+}
