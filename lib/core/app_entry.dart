adf28ed9a4c4ca94bd32bac2b11fb9adae561745
AnonX9 <avaikamarcel@gmail.com>
2024-03-23 18:18:23 +0100

    add routing to main entry

--- lib/core/app_entry.dart
+++ lib/core/app_entry.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_starter/router/router.dart';
 
-import '../src/home/presentation/home_page.dart';
-
-class AppEntry extends StatelessWidget {
+class AppEntry extends ConsumerWidget {
   const AppEntry({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_starter/logs/app_provider_observer.dart';
 import 'package:stack_trace/stack_trace.dart';
 
-import 'clients/local_db.client.dart';
 import 'clients/talker.dart';
 import 'core/app_entry.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await ObjectBoxManager.init(); // Initialize ObjectBoxManager
+  WidgetsFlutterBinding.ensureInitialized(); // Initialize ObjectBoxManager
 
   FlutterError.onError = (details) => talker.handle(
         details.exception,
