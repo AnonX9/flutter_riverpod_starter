324d426ac42b1a5325ee37754a2e01a77b488a5c
AnonX9 <avaikamarcel@gmail.com>
2024-06-18 19:34:11 +0100

    docs: :memo: Add doc to the SizedBoxExtension

--- lib/utils/extensions/app_extensions.dart
+++ lib/utils/extensions/app_extensions.dart
@@ -1,5 +1,18 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+/// Extensions for [num] that provide convenient access to [SizedBox] widgets.
+///
+/// The [SizedBox] widget is used to constrain the size of a child widget.
+/// This extension provides easy access to [SizedBox] widgets by providing
+/// properties for setting the height and width of the widget.
+///
+/// Example usage:
+///
+/// ```dart
+/// 100.height;
+/// 100.width;
+/// ```
+///
 extension SizedBoxExtention on num {
   SizedBox get height => SizedBox(
         height: toDouble(),
